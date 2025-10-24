1f2e39b34333a870f5419ef5a78ab7d19280e659
Rodrigo Rocha <rocha.rodrigogomes@gmail.com>
2025-10-24 13:21:54 -0300

    cria projeto tourai

---
+++ src/main/java/br/imd/ufrn/tourai/TouraiApplication.java
@@ -0,0 +1,13 @@
+package br.imd.ufrn.tourai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouraiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouraiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/imd/ufrn/tourai/TouraiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.imd.ufrn.tourai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouraiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
